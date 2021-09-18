88d36b4e4b130528765cee01028f8c18e98a38d6
Matilainen Olli E <matilaol@cubbli20-gold.local>
2021-09-19 01:13:39 +0300

    Add first unit test

---
+++ test/index.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const logic = require('../logic');
+
+describe('Simple Unit Test', () => {
+ it('Basic test', () => {
+        const wantedResult = [
+            'Maanantai 20.9.',
+            'Tiistai 21.9.',
+            'Keskiviikko 22.9.',
+            'Torstai 23.9.',
+            'Perjantai 24.9.',
+        ]
+        const date = new Date('Sep 18 2021 21:52:56 GMT+0300 (Eastern European Summer Time)');
+        const result = logic.generateNextWeek(date);
+        assert.equal(result.length, 5);
+        for (let i = 0; i < 5; i++) {
+            assert.equal(result[i], wantedResult[i]);
+        }
+    });
+});
