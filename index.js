b05ca6583284bed2e87e618256eb22bb181e6acb
kukaan <58886775+kukaan@users.noreply.github.com>
2021-09-20 21:33:36 +0300

    schedule next week's posts

--- index.js
+++ index.js
@@ -19,7 +19,42 @@ app.event('reaction_added', async ({ event, client }) => {
   console.log(`User <${event.user}> reacted`)
 });
 
+app.message('schedule next', async({ message, say }) => {
+  scheduleNextMessagesToAllMemberChannels()
+});
+
+
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
 })();
+
+
+async function scheduleNextMessagesToAllMemberChannels() {
+  const result = await app.client.conversations.list();
+  const date = new Date();
+  result.channels.forEach(channel => {
+    if (channel.is_member) {
+      console.log("member of channel", channel.name);
+
+      // set the time to post at 00:00 next Sunday
+      date.setDate(date.getDate() + logic.daysUntilMonday(date) - 1);
+      date.setHours(0, 0, 0, 0);
+
+      scheduleMessagesToOneChannel(channel.id, date);
+    }
+  })
+  console.log("posting at ",date);
+}
+
+async function scheduleMessagesToOneChannel(channelId, date) {
+  let i = 0
+  for (const lineToPrint of logic.generateNextWeek(new Date())) {
+    const result = await app.client.chat.scheduleMessage({
+      channel: channelId,
+      text: lineToPrint,
+      post_at: date.getTime() / 1000 + i++ // incrementing seconds seems necessary to keep the weekdays in order
+    });
+    console.log(result);
+  }
+}
