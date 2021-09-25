f43ff9565ff04729d2da4a61e85734da154417a4
kukaan <58886775+kukaan@users.noreply.github.com>
2021-09-25 17:01:24 +0300

    schedule indefinitely using a scheduling libary

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const schedule = require('node-schedule');
 const { App } = require('@slack/bolt');
 const logic = require('./logic');
 
@@ -19,16 +20,56 @@ app.event('reaction_added', async ({ event, client }) => {
   console.log(`User <${event.user}> reacted`)
 });
 
-app.message('schedule next', async({ message, say }) => {
-  scheduleNextMessagesToAllMemberChannels()
-});
-
-
 (async () => {
   await app.start(process.env.PORT || 3000);
+  startScheduling();
   console.log('⚡️ Bolt app is running!');
 })();
 
+// the new way using a scheduling library
+async function startScheduling() {
+  // doesn't work for private channels btw
+  (await app.client.conversations.list()).channels
+  .filter(c => c.is_member)
+  .forEach(c => {
+    const everySunday = new schedule.RecurrenceRule();
+    everySunday.dayOfWeek = 0 // 0 means Sunday
+
+    // for testing
+    //everySunday.dayOfWeek = 6
+    //everySunday.second = 40
+
+    console.log("scheduling posts to channel", c.name, "on dayOfWeek",everySunday.dayOfWeek)
+    weekdays = logic.generateNextWeek(new Date())
+    const job = schedule.scheduleJob(everySunday, () => {
+      // seems necessary to do something like this to post the weekdays in the same order
+      postMessage(c.id, weekdays[0])
+        .then(() => postMessage(c.id, weekdays[1]))
+        .then(() => postMessage(c.id, weekdays[2]))
+        .then(() => postMessage(c.id, weekdays[3]))
+        .then(() => postMessage(c.id, weekdays[4]))
+    })
+  })
+}
+
+/* this could separated from the above and used right before posting the messages
+async function updateChannelMembershipList() {
+} */
+
+async function postMessage(channelId, text) {
+  await app.client.chat.postMessage({
+    channel: channelId,
+    text: text
+  })
+}
+
+
+
+// the old way using the API method
+/*
+app.message('schedule next', async({ message, say }) => {
+  scheduleNextMessagesToAllMemberChannels()
+});
 
 async function scheduleNextMessagesToAllMemberChannels() {
   const result = await app.client.conversations.list();
@@ -58,3 +99,4 @@ async function scheduleMessagesToOneChannel(channelId, date) {
     console.log(result);
   }
 }
+*/
