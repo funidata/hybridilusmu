0a921fe317ac9ea87f38da8dd19fd4967f56b8fb
Tuomo Artama <tuomo.artama@gmail.com>
2021-09-27 14:38:40 +0300

    tietokantaa vaille valmis

--- logic.js
+++ logic.js
@@ -1,11 +1,12 @@
+const weekdays = [
+  'Maanantai',
+  'Tiistai',
+  'Keskiviikko',
+  'Torstai',
+  'Perjantai'
+]
+
 const generateNextWeek = day => {
-  const weekdays = [
-    'Maanantai',
-    'Tiistai',
-    'Keskiviikko',
-    'Torstai',
-    'Perjantai'
-  ]
   const daysUntilMonday = day.getDay() === 0 ? 1 : 8 - day.getDay()
   day.setDate(day.getDate() + daysUntilMonday)
   const result = []
@@ -16,4 +17,42 @@ const generateNextWeek = day => {
   return result
 }
 
-module.exports = { generateNextWeek };
+const generateWeek = (day) => {
+  const res = []
+  const currDate = new Date();
+  for (let i = 0; i < 7; i++) {
+    dayNumber = currDate.getDay();
+    if (dayNumber === 6 || dayNumber === 0) {
+      currDate.setDate(currDate.getDate() + 1)
+      continue
+    }
+    res.push(`${weekdays[dayNumber-1]} ${currDate.getDate()}.${currDate.getMonth() + 1}.`)
+    currDate.setDate(currDate.getDate() + 1)
+  }
+  return res;
+}
+
+const users = new Map();
+generateWeek(new Date()).forEach(d => users.set(d, []))
+
+const getEnrollmentsFor = (date) => {
+  return users.get(date)
+}
+
+const setInOffice = (userId, date) => {
+  users.set(date, users.get(date).concat(userId))
+}
+
+const setAsRemote = (userId, date) => {
+  users.set(date, users.get(date).filter(function(e){return e != userId}))
+}
+
+const userInOffice = (userId, date) => {
+  return users.get(date).includes(userId)
+}
+
+const userIsRemote = (userId, date) => {
+  return false
+}
+
+module.exports = { generateNextWeek, generateWeek, getEnrollmentsFor, setInOffice, setAsRemote, userInOffice, userIsRemote};
