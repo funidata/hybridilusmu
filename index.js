57d72a988875859f1a42572916db606e07d62387
Melker Narikka <meklu@meklu.org>
2021-10-03 13:19:09 +0300

    Fix scheduling

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function startScheduling() {
   onceEverySunday.tz = 'Etc/UTC';
   onceEverySunday.dayOfWeek = 0
   onceEverySunday.hour = 10
+  onceEverySunday.minute = 30
   console.log("scheduling posts to every public channel the bot is a member of on dayOfWeek",onceEverySunday.dayOfWeek,"at hour",onceEverySunday.hour,onceEverySunday.tz)
   const job = schedule.scheduleJob(onceEverySunday, () => {
     weekdays = logic.generateNextWeek(new Date())
