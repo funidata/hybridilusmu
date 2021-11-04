4ab4e77c0b4e482fb8d26555eff852ce3f8761d7
Tuomo Artama <tuomo.artama@gmail.com>
2021-11-04 16:27:03 +0200

    database eslintfix

--- database.js
+++ database.js
@@ -1,4 +1,3 @@
-const { SocketModeClient } = require('@slack/socket-mode');
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
@@ -9,7 +8,7 @@ const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
         port: process.env.DB_PORT || '5432',
         dialect: 'postgres',
         dialectOptions: {
-            ssl: process.env.DB_SSL == 'true',
+            ssl: process.env.DB_SSL === 'true',
         },
         logging: false,
     });
@@ -19,9 +18,9 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.Person = require('./models/person.model.js')(sequelize, Sequelize);
-db.Signup = require('./models/signup.model.js')(sequelize, Sequelize);
-db.Defaultsignup = require('./models/defaultsignup.model.js')(sequelize, Sequelize);
+db.Person = require('./models/person.model')(sequelize, Sequelize);
+db.Signup = require('./models/signup.model')(sequelize, Sequelize);
+db.Defaultsignup = require('./models/defaultsignup.model')(sequelize, Sequelize);
 
 db.Person.hasMany(db.Signup, { as: 'signups' });
 db.Person.hasMany(db.Defaultsignup, { as: 'defaultsignups' });
