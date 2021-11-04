c9a3de3c5372ae80c2ec88d9b1117871f537ed07
Tuomo Artama <tuomo.artama@gmail.com>
2021-11-04 16:14:14 +0200

    home eslintfix

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 const { DateTime } = require('luxon');
 const service = require('./databaseService');
 const dfunc = require('./dateFunctions');
-const { plain_text, mrkdwn } = require('./blocks/section');
+const { plainText, mrkdwn } = require('./blocks/section');
 const { header } = require('./blocks/header');
 const { actions } = require('./blocks/actions');
 const { divider } = require('./blocks/divider');
@@ -10,6 +10,67 @@ const { button } = require('./blocks/elements/button');
 const SHOW_DAYS_UNTIL = 10;
 const DAYS_IN_WEEK = 5;
 const format = { ...DateTime.DATETIME_MED, month: 'long' };
+
+const getDefaultSettingsBlock = async (userId) => {
+    const settingsBlock = [];
+    settingsBlock.push(mrkdwn('Oletusarvoisesti olen...'));
+    for (let i = 0; i < DAYS_IN_WEEK; i += 1) {
+        const weekday = dfunc.weekdays[i];
+        const buttonValue = {
+            weekday,
+            defaultAtOffice: await service.userAtOfficeByDefault(userId, weekday), // eslint-disable-line
+            defaultIsRemote: await service.userIsRemoteByDefault(userId, weekday), // eslint-disable-line
+        };
+        if (weekday === 'Keskiviikko') settingsBlock.push(mrkdwn(`*${weekday}isin*`));
+        else settingsBlock.push(mrkdwn(`*${weekday}sin*`));
+        settingsBlock.push(
+            actions([
+                button('Toimistolla', 'default_office_click', JSON.stringify(buttonValue), `${buttonValue.defaultAtOffice ? 'primary' : null}`),
+                button('Etänä', 'default_remote_click', JSON.stringify(buttonValue), `${buttonValue.defaultIsRemote ? 'primary' : null}`),
+            ]),
+        );
+    }
+    return settingsBlock;
+};
+
+const getUpdateBlock = async () => {
+    const updateBlock = [];
+    updateBlock.push(
+        plainText(`Tiedot päivitetty ${DateTime.now().setZone('Europe/Helsinki').setLocale('fi').toLocaleString(format)}`),
+        actions([button('Päivitä', 'update_click', 'updated')]),
+        divider(),
+    );
+    return updateBlock;
+};
+
+const getRegistrationsBlock = async (userId) => {
+    const registrationsBlock = [];
+    const dates = dfunc.listNWeekdays(DateTime.now(), SHOW_DAYS_UNTIL);
+    for (let i = 0; i < dates.length; i += 1) {
+        const date = dates[i];
+        registrationsBlock.push(header(dfunc.toPrettyFormat(date)));
+        const registrations = await service.getRegistrationsFor(date); // eslint-disable-line
+        let userIdList = registrations.length === 0 ? 'Kukaan ei ole ilmoittautunut toimistolle!' : 'Toimistolla aikoo olla:\n';
+        registrations.forEach((user) => {
+            userIdList += `<@${user}>\n`;
+        });
+        const buttonValue = {
+            date,
+            atOffice: await service.userAtOffice(userId, date), // eslint-disable-line
+            isRemote: await service.userIsRemote(userId, date), // eslint-disable-line
+        };
+        registrationsBlock.push(
+            mrkdwn(userIdList),
+            plainText('Oma ilmoittautumiseni:'),
+            actions([
+                button('Toimistolla', 'office_click', JSON.stringify(buttonValue), `${buttonValue.atOffice ? 'primary' : null}`),
+                button('Etänä', 'remote_click', JSON.stringify(buttonValue), `${buttonValue.isRemote ? 'primary' : null}`),
+            ]),
+            divider(),
+        );
+    }
+    return registrationsBlock;
+};
 
 /**
  * Updates the App-Home page.
@@ -28,67 +89,6 @@ const update = async (client, userId) => {
             blocks,
         },
     });
-};
-
-const getDefaultSettingsBlock = async (userId) => {
-    const settingsBlock = [];
-    settingsBlock.push(mrkdwn('Oletusarvoisesti olen...'));
-    for (let i = 0; i < DAYS_IN_WEEK; i++) {
-        const weekday = dfunc.weekdays[i];
-        const buttonValue = {
-            weekday,
-            defaultAtOffice: await service.userAtOfficeByDefault(userId, weekday),
-            defaultIsRemote: await service.userIsRemoteByDefault(userId, weekday),
-        };
-        if (weekday === 'Keskiviikko') settingsBlock.push(mrkdwn(`*${weekday}isin*`));
-        else settingsBlock.push(mrkdwn(`*${weekday}sin*`));
-        settingsBlock.push(
-            actions([
-                button('Toimistolla', 'default_office_click', JSON.stringify(buttonValue), `${buttonValue.defaultAtOffice ? 'primary' : null}`),
-                button('Etänä', 'default_remote_click', JSON.stringify(buttonValue), `${buttonValue.defaultIsRemote ? 'primary' : null}`),
-            ]),
-        );
-    }
-    return settingsBlock;
-};
-
-const getUpdateBlock = async () => {
-    const updateBlock = [];
-    updateBlock.push(
-        plain_text(`Tiedot päivitetty ${DateTime.now().setZone('Europe/Helsinki').setLocale('fi').toLocaleString(format)}`),
-        actions([button('Päivitä', 'update_click', 'updated')]),
-        divider(),
-    );
-    return updateBlock;
-};
-
-const getRegistrationsBlock = async (userId) => {
-    const registrationsBlock = [];
-    const dates = dfunc.listNWeekdays(DateTime.now(), SHOW_DAYS_UNTIL);
-    for (let i = 0; i < dates.length; i++) {
-        const date = dates[i];
-        registrationsBlock.push(header(dfunc.toPrettyFormat(date)));
-        const registrations = await service.getRegistrationsFor(date);
-        let userIdList = registrations.length === 0 ? 'Kukaan ei ole ilmoittautunut toimistolle!' : 'Toimistolla aikoo olla:\n';
-        registrations.forEach((user) => {
-            userIdList += `<@${user}>\n`;
-        });
-        const buttonValue = {
-            date,
-            atOffice: await service.userAtOffice(userId, date),
-            isRemote: await service.userIsRemote(userId, date),
-        };
-        registrationsBlock.push(
-            mrkdwn(userIdList),
-            plain_text('Oma ilmoittautumiseni:'),
-            actions([
-                button('Toimistolla', 'office_click', JSON.stringify(buttonValue), `${buttonValue.atOffice ? 'primary' : null}`),
-                button('Etänä', 'remote_click', JSON.stringify(buttonValue), `${buttonValue.isRemote ? 'primary' : null}`),
-            ]),
-            divider(),
-        );
-    }
-    return registrationsBlock;
 };
 
 const error = async (client, userId, message) => {
