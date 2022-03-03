c0bbb925cf39740f32fd5cf38e9e4f9fefcad358
Tommi Pulli <tommi.pulli@funidata.fi>
2022-03-03 13:50:03 +0200

    add initial models and migrations created with sequelize-cli

---
+++ src/models/signup.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Signup extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.Person);
+    }
+  }
+  Signup.init({
+    officeDate: DataTypes.DATEONLY,
+    atOffice: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Signup',
+    indexes: [{ fields: ['officeDate'] }]
+  });
+  return Signup;
+};
