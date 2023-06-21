aac3ef1fb0fe91f8505b340b3afe0fa16365241f
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-06-21 09:49:46 +0300

    Fix env var configuration both locally and in CI

--- e2e/cypress/test-test.cy.ts
+++ e2e/cypress/test-test.cy.ts
@@ -1,11 +1,11 @@
 describe("Testing tests", () => {
   it("Asd", () => {
-    // cy.visit('')
+    cy.visit("");
     // Must use Cypress.env('baseUrl') as Cypress has a bug in resolving baseUrl from cypress.env.json files.
     // No problem because we will need a custom visit/login function anyways.
     const acualBaseUrl = Cypress.env("baseUrl");
     console.log(acualBaseUrl);
-    cy.visit(acualBaseUrl);
+    //cy.visit(acualBaseUrl);
     console.log("asd");
     cy.contains("Sign in to");
   });
