bdc822565ae28a1216688b0620ba2219d0dbe152
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-07-18 15:50:31 +0300

    Setup vuepress

---
+++ docs/src/.vuepress/config.ts
@@ -0,0 +1,14 @@
+import { defaultTheme, defineUserConfig } from "vuepress";
+
+export default defineUserConfig({
+  dest: "build",
+  lang: "en-US",
+  title: "Hybridilusmu Documentation",
+  head: [["link", { rel: "icon", href: "/logo/favicon.png" }]],
+  theme: defaultTheme({
+    repo: "funidata/hybridilusmu",
+    logo: "/logo/hybridilusmulogo1whitebgsmall.jpg",
+    logoDark: "/logo/hybridilusmulogo1darkbgsmall.jpg",
+    docsDir: "docs",
+  }),
+});
