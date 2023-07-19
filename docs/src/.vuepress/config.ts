849fda71e5edfe733d01e65a977b56843b93e671
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-07-19 10:36:31 +0300

    Fix vuepress base path

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import { defaultTheme, defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
+  base: "/hybridilusmu/",
   dest: "build",
   lang: "en-US",
   title: "Hybridilusmu Documentation",
