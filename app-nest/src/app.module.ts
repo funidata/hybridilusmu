cff129a755ed4982b33c8584ddac7a88ef1c94be
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-07-26 13:09:07 +0300

    Add common configuration object

--- app-nest/src/app.module.ts
+++ app-nest/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from "@nestjs/common";
-
 import { BoltModule } from "./bolt/bolt.module";
+import configuration from "./config/configuration";
 
 @Module({
   imports: [
     BoltModule.register({
-      token: process.env.SLACK_BOT_TOKEN,
-      appToken: process.env.SLACK_APP_TOKEN,
-      signingSecret: process.env.SLACK_SIGNING_SECRET,
+      token: configuration.bolt.token,
+      appToken: configuration.bolt.appToken,
+      signingSecret: configuration.bolt.signingSecret,
     }),
   ],
 })
---
+++ app-nest/src/config/configuration.ts
@@ -0,0 +1,16 @@
+const configuration = {
+  port: parseInt(process.env.PORT) || 3000,
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT) || 5432,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  },
+  bolt: {
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+  },
+};
+
+export default configuration;
