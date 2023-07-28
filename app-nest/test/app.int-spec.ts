52345061216345edddd927e52d8a8938d187f829
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-07-28 15:48:30 +0300

    Refactor

---
+++ app-nest/test/app.int-spec.ts
@@ -0,0 +1,23 @@
+import { INestApplication } from "@nestjs/common";
+import { UserService } from "../src/entities/user/user.service";
+import createTestingModule from "./test-app.module";
+
+describe("App", () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture = await createTestingModule().compile();
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("Find all users", async () => {
+    const userService = app.get(UserService);
+    const users = await userService.findAll();
+    expect(users).toHaveLength(1);
+  });
+});
