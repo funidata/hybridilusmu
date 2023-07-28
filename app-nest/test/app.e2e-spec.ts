69d0a6487ae1ff90089eb33231a44932c49a9d34
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-07-28 12:25:09 +0300

    Working integration test prototype

--- app-nest/test/app.e2e-spec.ts
+++ app-nest/test/app.e2e-spec.ts
@@ -1,24 +1,60 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { DiscoveryModule } from "@golevelup/nestjs-discovery";
+import { createMock } from "@golevelup/ts-jest";
+import { INestApplication, Module } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BoltModule } from "../src/bolt/bolt.module";
+import { BoltService } from "../src/bolt/bolt.service";
+import { EntitiesModule } from "../src/entities/entities.module";
+import { UserService } from "../src/entities/user/user.service";
+import { GuiModule } from "../src/gui/gui.module";
 
-describe('AppController (e2e)', () => {
+@Module({
+  imports: [DiscoveryModule],
+  providers: [],
+  exports: [createMock<BoltService>],
+})
+class MockBolt {}
+
+describe("AppController (e2e)", () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      // imports: [AppModule],
+      imports: [
+        TypeOrmModule.forRoot({
+          type: "postgres",
+          host: "localhost",
+          username: "postgres",
+          password: "postgres",
+          database: "test",
+          synchronize: true,
+          autoLoadEntities: true,
+        }),
+        GuiModule,
+        EntitiesModule,
+      ],
+    })
+      .overrideModule(BoltModule)
+      .useModule(MockBolt)
+      // .overrideProvider(BoltService)
+      // .useClass(createMock<BoltService>())
+      // .overrideProvider(BoltRegisterService)
+      // .useClass(createMock<BoltRegisterService>())
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("Find all users", async () => {
+    const userService = app.get(UserService);
+    const users = await userService.findAll();
+    expect(users).toHaveLength(1);
   });
 });
